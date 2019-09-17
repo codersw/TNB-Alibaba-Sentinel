cebea8d7fff2ca8337553b25921b7f9f97853331
swen <1063642821@qq.com>
2019-09-17 15:47:08 +0800

    加一个test请求测试压力

---
+++ dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.alibaba.csp.sentinel.dashboard.controller;
+
+import com.alibaba.csp.sentinel.dashboard.domain.Result;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/test")
+    public Result test(){
+        return Result.ofSuccess("");
+    }
+}
---
+++ gateway/src/main/java/com/mango/gateway/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.mango.gateway.controller;
+
+import com.mango.gateway.model.GatewayResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/test")
+    public GatewayResponse test(){
+        return GatewayResponse.builder().build();
+    }
+}
